6ec569c5cbdf31061ca333affffb521d39a4a6d8
nateGeorge <wordsforthewise@gmail.com>
2019-02-05 09:56:47 -0700

    fix file search

--- unzip_files.sh
+++ unzip_files.sh
@@ -11,7 +11,7 @@ fi
 # tricky to do:
 # http://www.ducea.com/2009/03/05/bash-tips-if-e-wildcard-file-check-too-many-arguments/
 files=$(ls Loan* 2> /dev/null | wc -l)
-if [ **"$files" != "0"** ]; then
+if [ "$files" != "0" ]; then
     for f in Loan*; do mv $f accept/; done
     cd accept
     for f in *.zip; do unzip $f; rm $f; done
@@ -20,7 +20,7 @@ else
 fi
 
 files=$(ls Reject* 2> /dev/null | wc -l)
-if [ **"$files" != "0"** ]; then
+if [ "$files" != "0" ]; then
     for f in Reject*; do mv $f reject/; done
     cd ../reject
     for f in *.zip; do unzip $f; rm $f; done
